62b2a8dfd13429662cb8f7aa6ee043c84bdf7801
Ricardo Biehl Pasquali <ricardo@pasqua.li>
2022-08-29 17:18:10 -0300

    Rewrite pcm.sh for full POSIX compliance
    Now the hardware parameters are generated directly in binary format by
    POSIX's printf utility with "%b" conversion specifier. Usage of xxd
    (which is not part of POSIX) was removed.

--- pcm.sh
+++ pcm.sh
@@ -6,116 +6,97 @@
 #
 # Ricardo Biehl Pasquali
 
+main() {
+	# Default parameters
+	bits_per_sample=16
+	channels=2
+	rate=44100
+
+	debug=false
+
+	# Read command line options
+	while getopts b:c:dr: name; do
+		case $name in
+			b) bits_per_sample="$OPTARG";;
+			c) channels="$OPTARG";;
+			r) rate="$OPTARG";;
+			d) debug=true;;
+			?) printf "Usage: %s: [-b sample_bits] [-c channels] [-r rate]\n" $0
+			exit 2;;
+		esac
+	done
+	shift $(($OPTIND - 1))
+
+	# If debug is true, print hardware parameters
+	if $debug; then get_hwparams | xxd; exit 0; fi
+
+	# Set hardware parameters
+	get_hwparams | ./ioctl $ALSA_DRIVER $HW_PARAMS 608 rw 2>/dev/null
+
+	# Prepare device
+	./ioctl $ALSA_DRIVER $PREPARE
+
+	cat
+
+	# Wait for PCM buffer to drain
+	./ioctl $ALSA_DRIVER $DRAIN
+}
+
+# Binary output helpers
+# ==============================================================================
+
+# little-endian by default
+print_bytes() (
+	bits=$1; val=$2; if [ -z $val ]; then val=0; fi
+    i=0; while [ $i -lt $bits ]; do
+        printf "%b" $(printf "\\%o" $((($val >> i) & 0xff))); i=$((i+8))
+    done
+)
+s8()  { print_bytes 8  $1; }
+s16() { print_bytes 16 $1; }
+s32() { print_bytes 32 $1; }
+
+# ALSA helpers
+# ==============================================================================
+
 ALSA_DRIVER="65" # numerical value for character 'A'
-HW_PARAMS="17"
-PREPARE="64"
-DRAIN="68"
+HW_REFINE="17" HW_PARAMS="17" PREPARE="64" DRAIN="68"
 
-get_hwparams_initialized ()
-{
-	# set flags to zero
-	i=1
-	while [ $i -gt 0 ]; do
-		printf "%08x\n"
-		i=$(($i+1))
-	done
+mask() { s32 $1; s32; s32; s32; s32; s32; s32; s32; }
+mask_val() { mask $((1 << $1)); }
+mask_any() { mask $((~0)); }
 
-	# fill all masks with ones (ffff)
-	i=8
-	while [ $i -gt 0 ]; do
-		printf "%064x\n" | tr "0" "f"
-		i=$(($i+1))
-	done
+interval() { s32 $1; s32 $2; s32; }
+interval_val() { interval $1 $1; }
+interval_any() { interval 0 $((~0)); }
 
-	# fill all intervals with full range
-	i=21
-	while [ $i -gt 0 ]; do
-		printf "%08x"              # min
-		printf "%08x" | tr "0" "f" # max
-		printf "%08x\n"            # flags
-		i=$(($i+1))
-	done
+# The order of commands does matter
+get_hwparams() {
+	s32 # hwparams flags
+
+	mask_val 3 # Access type
+	mask_any # Format
+	mask_any # Subformat
+
+	i=5; while [ $((i--)) -gt 0 ]; do mask_any; done
+
+	interval_val $bits_per_sample # Sample bits
+	interval_any # Frame bits
+	interval_val $channels # Channels
+	interval_val $rate # Rate
+	interval_any # Period time (us)
+	interval_any # Period size (frames)
+	interval_any # Period bytes
+	interval_any # Periods
+	interval_any # Buffer time (us)
+	interval_any # Buffer size (frames)
+	interval_any # Buffer bytes
+	interval_any # Tick time (us)
+
+	i=9; while [ $((i--)) -gt 0 ]; do interval_any; done
 
 	# fill remaining integers
-	i=24
-	while [ $i -gt 0 ]; do
-		printf "%08x\n"
-		i=$(($i+1))
-	done
+	i=24; while [ $((i--)) -gt 0 ]; do s32; done
 }
 
-# In hexadecimal format, two characters represent one byte.
-# For splitting bytes, just add a newline each two characters :-)
-split_bytes ()
-{
-	fold -w 2
-}
-
-# Converts a continuous hexadecimal string from standard input into
-# little-endian and writes to standard output.
-as_little_endian ()
-( # runs in a subshell
-	out=""
-	for byte in $(split_bytes); do
-		out="$byte$out";
-	done
-	printf "$out"
-)
-
-to_hex ()
-{
-	printf "%08x" $1 | as_little_endian
-}
-
-setparam() {
-	param_id=$1; min=$(to_hex $2); max=$(to_hex $2)
-	sed "${param_id}s/[0-9a-fA-F]\{16\}/${min}${max}/"
-}
-
-# Get parameters from command line
-# ================================
-
-# Default parameters
-bits_per_sample=16
-channels=2
-rate=44100
-
-while getopts b:c:r: name; do
-	case $name in
-		b) bits_per_sample="$OPTARG";;
-		c) channels="$OPTARG";;
-		r) rate="$OPTARG";;
-		?) printf "Usage: %s: [-b sample_bits] [-c channels] [-r rate]\n" $0
-		   exit 2;;
-	esac
-done
-
-shift $(($OPTIND - 1))
-#printf "Remaining arguments are: %s\n" "$*"
-
-# Prepare hardware parameters buffer
-# ==================================
-
-# Initialize buffer to empty string
-buf="$(get_hwparams_initialized)"
-
-# sets the access mode to Read/Write Interleaved
-val=$(printf "%064x" $((1 << 3)) | as_little_endian)
-buf=$(echo "$buf" | sed "2s/.*/$val/")
-
-SAMPLE_BITS=10 CHANNELS=12 RATE=13
-buf=$(echo $buf | \
-	setparam $SAMPLE_BITS $bits_per_sample | \
-	setparam $CHANNELS    $channels | \
-	setparam $RATE        $rate )
-
-# Set hardware parameters
-echo "$buf" | xxd -r -p - | ./ioctl $ALSA_DRIVER $HW_PARAMS 608 rw 2>/dev/null
-
-# Prepare device
-./ioctl $ALSA_DRIVER $PREPARE
-
-cat
-
-# Wait for PCM buffer to drain
-./ioctl $ALSA_DRIVER $DRAIN
+main $@
